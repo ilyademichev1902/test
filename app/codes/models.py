c5eba82bf1fb684872626d01ec723677626cba46
ilyademichev1902 <ilyademichev1902@ya.ru>
2023-09-15 08:35:54 +0700

    5 commit

--- app/codes/models.py
+++ app/codes/models.py
@@ -4,8 +4,8 @@ from flask import current_app
 class Airport(current_app.config['db'].Model):
     __tablename__ = 'airport'
     id = Column(Integer, primary_key=True)
-    code = Column(String(4), unique=True)
-    ru_airport_name = Column(String(65))
-    en_airport_name = Column(String(65))
+    code = Column("Код",String(4), unique=True)
+    ru_airport_name = Column("Название(русский)",String(65))
+    en_airport_name = Column("Название(англ)",String(65))
 
 
