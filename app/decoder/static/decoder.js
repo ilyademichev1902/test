91911cce4f31bd1d2ccd954e45e0ef237ce87242
ilyademichev1902 <ilyademichev1902@ya.ru>
2023-10-27 18:54:24 +0800

    formatter by columns

--- app/decoder/static/decoder.js
+++ app/decoder/static/decoder.js
@@ -1,4 +1,9 @@
- 
+const valid_flight=/(?:(?<=^)|(?<=\.)|(?<=\s))[A-Z,А-Я][A-Z,А-Я,1-9]\s{0,1}[ ,-]{0,1}\d{3,4}(?=\s|$)/g;
+const valid_route=/(?:(?<=^)|(?<=\.)|(?<=\s))(([A-Z,А-ЯЁ]{3,4})([A-Z,А-ЯЁ]{3,4}))(?=\s|$)/g;
+const valid_date=/(?:(?<=^)|(?<=\.)|(?<=\s))((\d{2})([A-Z,А-ЯЁ]{3})(\d{2}){0,1})(?=\s|$)/g;
+const valid_time=/(?:(?<=^)|(?<=\.)|(?<=\s))(\d{4})\s{1,}(\d{4})(\+1){0,1}(?=\s|$)/g;
+const valid_regex = [valid_flight,valid_route,valid_date,valid_time];
+
 /*function instant_decoder(filled_form){
 	
 	console.log("change");
@@ -14,32 +19,50 @@
 	});
 }*/
 
+async function pnr_string_is_valid(pnr_string){
+	//if any fails
+	//for ( const v of valid_regex ) {
+	//		console.log ( ( pnr_string.match(v) 	|| [] ).length  ) ;
+	//};//else
+	return valid_regex.every(vr => (pnr_string.match(vr) || []).length == 1);
+}
+
 //calls decoder once textbox changes    
 async function instant_decoder(filled_form){
-	var codes =  document.getElementById("gds_input").value;
-	console.log(codes);
+	var valid_pnr_strings = [];
+	var codes =  document.getElementById("gds_input").value.split(/\r?\n/);
+	//console.log(codes);
 	//are there any valid PNR strings?
-	if ((codes.match(valid_pnr) || []).length == 0) {
+	for (const current_string of codes ){
+		let is_valid = await pnr_string_is_valid(current_string);
+		if (is_valid)
+		{
+			console.log(current_string);
+			valid_pnr_strings.push(current_string);
+		}
+	}
+//	console.log(valid_pnr_strings);
+	if (valid_pnr_strings.length == 0){
 		document.getElementById("decoder_out").value = "Ожидаю корректные данные";
 		return;
 	}
 	//get the set language and launch the converter
 	//console.log(base_url)
-	parts = base_url.split('/');
+	let parts = base_url.split('/');
 	//console.log(parts);
-	urla = parts.slice(0,parts.length-2);
+	let urla = parts.slice(0,parts.length-2);
 	//console.log(urla);
-	url = urla.join('/');
+	let url = urla.join('/');
 	//console.log(url);
-	lang_selector = document.getElementById("form-select");
-	language_selected = lang_selector.value;
+	let lang_selector = document.getElementById("form-select");
+	let language_selected = lang_selector.value;
 	//API url for selected language
-	codes_url = url.concat('/', language_selected ).concat('/');	
+	let codes_url = url.concat('/', language_selected ).concat('/');	
 	//console.log(codes_url);
 	//codes = document.getElementById("gds_input").value = "Идет декодирование...";	
 	//console.log(codes);
 	document.getElementById("decoder_out").value = "Идет декодирование...";	
-	codes_in_json = {
+	let codes_in_json = {
 		headers: {
 		'Content-Type': 'application/json'
 		},
@@ -47,6 +70,7 @@ async function instant_decoder(filled_form){
 		body: JSON.stringify(valid_pnr_strings)
 	}
 	console.log(codes_in_json);
+	response = null;
 	try{
 		response = await fetch(codes_url,codes_in_json);
 		if (!response.ok) throw "Bad response"; 
@@ -59,20 +83,23 @@ async function instant_decoder(filled_form){
 	let decoded_data = await response.json();	
 	if ((decoded_data.errors || []).length > 0){
 		console.log(decoded_data.errors);
-		errors_lines = decoded_data.errors.join('\r\n');
+		let errors_lines = decoded_data.errors.join('\r\n');
 		console.log(errors_lines);
 		document.getElementById("decoder_out").value = errors_lines;
 		return;
 	}			
-	//console.log(decoded_data.data);
-	document.getElementById("decoder_out").value  = decoded_data.data.join('\r\n');
+	else{
+		console.log(decoded_data.data);
+		document.getElementById("decoder_out").value  = decoded_data.data.join('\r\n');
+		console.log(decoded_data.data.join('\r\n'));
 
+	 }
 }
 
 window.addEventListener('load', (event) => {
-	parts = base_url.split('/');
-	default_language  = parts[parts.length-2];
-	lang_selector = document.getElementById("form-select");
+	let parts = base_url.split('/');
+	let default_language  = parts[parts.length-2];
+	let lang_selector = document.getElementById("form-select");
 	lang_selector.value = default_language;
 	//console.log(default_language);
 	document.getElementById("decoder_out").value = "Ожидаю корректные данные";
